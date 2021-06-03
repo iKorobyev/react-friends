ad6d11a7ced4182baab6e3534c1a3dfa7573e9f0
Igor <iKorobyev@google.com>
2021-06-04 00:17:53 +0300

    Adding for deploy.

---
+++ build/precache-manifest.eee5a800dd1cd534d5272e4ef6ccbba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465b58e258f1412f06d875d0b938f9aa",
+    "url": "/react-friends/index.html"
+  },
+  {
+    "revision": "34eb4e17402b8336e31a",
+    "url": "/react-friends/static/css/2.4fc72a86.chunk.css"
+  },
+  {
+    "revision": "fa1dc9d5ae53186bd0a6",
+    "url": "/react-friends/static/css/main.71c007cd.chunk.css"
+  },
+  {
+    "revision": "34eb4e17402b8336e31a",
+    "url": "/react-friends/static/js/2.a4e41896.chunk.js"
+  },
+  {
+    "revision": "fa1dc9d5ae53186bd0a6",
+    "url": "/react-friends/static/js/main.3c6e3607.chunk.js"
+  },
+  {
+    "revision": "542228243269c63a9ea5",
+    "url": "/react-friends/static/js/runtime~main.4e3ecc9d.js"
+  }
+]);
